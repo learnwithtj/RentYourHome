ef30b1db669f7487de0f1257479c439b53b70756
learnwithtj <987449@win.edu.au>
2024-10-15 01:52:25 +1100

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+<footer class="full-row bg-secondary p-0">
+            <div style="text-align:center;" class="container">
+                
+                <div style="text-align:center;" class="row copyright">
+                    <div style="text-align:center;" class="col-sm-6"> <span class="text-white">© <?php echo date('Y');?> All Rights Reserved to RentYourHome.com.au </span> </div>
+                
+                    </div>
+                </div>
+            </div>
+        </footer>
